baf1e21b47c78be4492b422061ec546509276a39
Dilum Aluthge <dilum@aluthge.com>
2019-12-13 07:25:22 -0500

    Add initial functionality to the package (#1)

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 [![Build Status](https://travis-ci.com/bcbi/GitCommand.jl.svg?branch=master)](https://travis-ci.com/bcbi/GitCommand.jl)
 [![Codecov](https://codecov.io/gh/bcbi/GitCommand.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/bcbi/GitCommand.jl)
+
+GitCommand allows you to use command-line Git in your Julia packages. You do
+not need to have Git installed on your computer, and neither do the users of
+your packages! GitCommand provides a Git binary via
+[Git_jll](https://github.com/JuliaBinaryWrappers/Git_jll.jl).
+Git_jll uses the Pkg Artifacts system, and therefore Git_jll and GitCommand
+require at least Julia 1.3.
+
+# Example usage
+
+```julia
+using GitCommand
+git_cmd() do git_path
+    run(`$(git_path) clone https://github.com/JuliaRegistries/General`)
+end
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
 using GitCommand
 using Test
 
+function with_temp_dir(f::Function)
+    original_directory = pwd()
+    tmp_dir = mktempdir()
+    atexit(() -> rm(tmp_dir; force = true, recursive = true))
+    cd(tmp_dir)
+    result = f(tmp_dir)
+    cd(original_directory)
+    rm(tmp_dir; force = true, recursive = true)
+    return result
+end
+
 @testset "GitCommand.jl" begin
-    # Write your own tests here.
+    if Sys.iswindows()
+#         @test GitCommand._separator() == ';'
+    else
+        @test GitCommand._separator() == ':'
+    end
+    with_temp_dir() do tmp_dir
+        @test !isdir("General")
+        @test !isfile(joinpath("General", "Registry.toml"))
+        GitCommand.git_cmd() do git_path
+            run(`$(git_path) clone https://github.com/JuliaRegistries/General`)
+        end
+        @test isdir("General")
+        @test isfile(joinpath("General", "Registry.toml"))
+    end
 end
