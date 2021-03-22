64d6cff67b2fe3f7050d8108183139ecd3ab4f58
Mosè Giordano <giordano@users.noreply.github.com>
2021-03-22 01:18:14 +0000

    Use `addenv` to ensure git commands don't leak env variables (#45)

--- src/git.jl
+++ src/git.jl
@@ -1,14 +1,8 @@
 function _git_cmd(str::AbstractString;
                   adjust_PATH::Bool = true,
                   adjust_LIBPATH::Bool = true)
-    git_path, env_mapping = _env_mapping(; adjust_PATH = adjust_PATH,
-                                           adjust_LIBPATH = adjust_LIBPATH)
-    new_env = copy(ENV)
-    for p in env_mapping
-        new_env[p[1]] = p[2]
-    end
-    new_cmd = Cmd(`$(git_path) $(split(str))`; env = new_env)
-    return new_cmd
+    git_path, env_mapping = _env_mapping(; adjust_PATH, adjust_LIBPATH)
+    return addenv(`$(git_path) $(split(str))`, env_mapping)
 end
 
 macro git_cmd(ex)
@@ -25,12 +19,16 @@ function _gitrepl_parser(repl_input::AbstractString)
     end
 end
 
+function git(;
+             adjust_PATH::Bool = true,
+             adjust_LIBPATH::Bool = true)
+    git_path, env_mapping = _env_mapping(; adjust_PATH, adjust_LIBPATH)
+    return addenv(`$(git_path)`, env_mapping...)
+end
+
+# This function should be deprecated, it's kept only for backward-compatibility
 function git(f::Function;
              adjust_PATH::Bool = true,
              adjust_LIBPATH::Bool = true)
-    git_path, env_mapping = _env_mapping(; adjust_PATH = adjust_PATH,
-                                           adjust_LIBPATH = adjust_LIBPATH)
-    return withenv(env_mapping...) do
-        return f(git_path)
-    end
+    return f(git(; adjust_PATH, adjust_LIBPATH))
 end
