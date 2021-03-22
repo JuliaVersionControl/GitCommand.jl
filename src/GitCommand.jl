043eb5e293c3070c3b7505463f5b1db8104fc9d1
Dilum Aluthge <dilum@aluthge.com>
2021-03-22 15:33:00 -0400

    Remove all uses of functions that use `withenv` (#46)
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    Co-authored-by: Eric Hanson <5846501+ericphanson@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    Co-authored-by: Eric Hanson <5846501+ericphanson@users.noreply.github.com>

--- src/GitCommand.jl
+++ src/GitCommand.jl
@@ -1,6 +1,5 @@
 module GitCommand
 
-import JLLWrappers
 import Git_jll
 import ReplMaker
 
@@ -12,9 +11,7 @@ const GIT_REPL_MODE_NAME = "GitCommand.jl Git REPL mode"
 const GIT_REPL_MODE_PROMPT_TEXT = "git> "
 const GIT_REPL_MODE_START_KEY = ','
 
-include("env.jl")
 include("git.jl")
 include("repl.jl")
-include("utils.jl")
 
 end # end module GitCommand
--- src/git.jl
+++ src/git.jl
@@ -1,32 +1,41 @@
 function _git_cmd(str::AbstractString;
                   adjust_PATH::Bool = true,
                   adjust_LIBPATH::Bool = true)
-    git_path, env_mapping = _env_mapping(; adjust_PATH, adjust_LIBPATH)
-    return addenv(`$(git_path) $(split(str))`, env_mapping)
+    cmd = git(; adjust_PATH, adjust_LIBPATH)
+    return `$(cmd) $(split(str))`
 end
 
 macro git_cmd(ex)
     return _git_cmd(ex)
 end
 
-function _gitrepl_parser(repl_input::AbstractString)
-    return quote
-        GitCommand.git() do git
-            repl_input = $(Expr(:quote, repl_input))
-            run(`$(git) $(split(repl_input))`)
-            return nothing
-        end
+function git(; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
+    @static if Sys.iswindows()
+        return Git_jll.git(; adjust_PATH, adjust_LIBPATH)::Cmd
+    else
+        root = Git_jll.artifact_dir
+
+        libexec = joinpath(root, "libexec")
+        libexec_git_core = joinpath(libexec, "git-core")
+
+        share = joinpath(root, "share")
+        share_git_core = joinpath(share, "git-core")
+        share_git_core_templates = joinpath(share_git_core, "templates")
+
+        ssl_cert = joinpath(dirname(Sys.BINDIR), "share", "julia", "cert.pem")
+
+        env_mapping = Dict{String,String}()
+        env_mapping["GIT_EXEC_PATH"]    = libexec_git_core
+        env_mapping["GIT_SSL_CAINFO"]   = ssl_cert
+        env_mapping["GIT_TEMPLATE_DIR"] = share_git_core_templates
+
+        original_cmd = Git_jll.git(; adjust_PATH, adjust_LIBPATH)::Cmd
+        return addenv(original_cmd, env_mapping...)::Cmd
     end
 end
 
-function git(;
-             adjust_PATH::Bool = true,
-             adjust_LIBPATH::Bool = true)
-    git_path, env_mapping = _env_mapping(; adjust_PATH, adjust_LIBPATH)
-    return addenv(`$(git_path)`, env_mapping...)
-end
-
 # This function should be deprecated, it's kept only for backward-compatibility
+# We will remove it soon in a breaking release
 function git(f::Function;
              adjust_PATH::Bool = true,
              adjust_LIBPATH::Bool = true)
