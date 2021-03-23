150dbd1a84153aa8a796c4b83c69810bce8c60e5
Dilum Aluthge <dilum@aluthge.com>
2021-03-22 20:06:00 -0400

    Reorganize some source code files

--- src/git.jl
+++ src/git.jl
@@ -1,14 +1,3 @@
-function _git_cmd(str::AbstractString;
-                  adjust_PATH::Bool = true,
-                  adjust_LIBPATH::Bool = true)
-    cmd = git(; adjust_PATH, adjust_LIBPATH)
-    return `$(cmd) $(split(str))`
-end
-
-macro git_cmd(ex)
-    return _git_cmd(ex)
-end
-
 function git(; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
     @static if Sys.iswindows()
         return Git_jll.git(; adjust_PATH, adjust_LIBPATH)::Cmd
